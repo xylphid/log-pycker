c455ca9844bf91055c0cd50a6c3b2843eb96c767
Anthony PERIQUET <anthony@periquet.net>
2018-07-30 15:04:41 +0200

    Fix process, test connection

--- app/helper/elasticsearch.py
+++ app/helper/elasticsearch.py
@@ -13,21 +13,36 @@ class ElasticHelper:
     es = None
 
     def __init__(self):
-        alive = False
-        if self.conf["host"] is None:
-            print("%s - ERROR - ElasticSearch host is not defined" % datetime.now())
-        else:
-            while not alive:
-                try:
-                    self.es = Elasticsearch(hosts=self.conf["host"].split(','))
-                    alive = True
-                except:
-                    print("Could not reach Elastic Searh. Will retry in 5 seconds ...")
-                    time.sleep(5)
+        self.checkHosts()
 
 
     def register(self, message):
         # print( json.dumps(self.conf) )
+        self.waitUntilAlive()
         if self.conf["host"] is not None:
             res = self.es.index(index=self.conf["name"], doc_type=self.conf["type"], body=message)
             # print( "ElasticSearch result : %s" % res )
+
+    def checkHosts(self):
+        if self.conf["host"] is None:
+            print("%s - ERROR - ElasticSearch host is not defined" % datetime.now())
+            return False
+        else:
+            return True
+
+    def waitUntilAlive(self):
+        if not self.checkHosts():
+            return False
+        else:
+            alive = False
+            while not alive:
+                try:
+                    self.es = Elasticsearch(hosts=self.conf["host"].split(','))
+                    alive = self.es.ping()
+                    if not alive:
+                        raise
+                except:
+                    print("Could not reach Elastic Searh. Will retry in 5 seconds ...")
+                    time.sleep(5)
+
+            return True
