dfdeb8c2addf1ed9e1810ca140d8e263fdd41289
Anthony PERIQUET <anthony@periquet.net>
2018-07-30 15:05:39 +0200

    Fix program loop, Update messages, Fix cleaning

--- app/app.py
+++ app/app.py
@@ -1,121 +1,28 @@
 #!/usr/local/bin/python
 
-# from abc import ABC, abstractmethod
-# from datetime import datetime
 import docker
 from helper.docker import DockerHelper, ContainerHelper
 from helper.elasticsearch import ElasticHelper
 from threading import RLock
-# import json
 import logging
 import os
-# import pika # RabbitMQ
-# import re
 import signal
 import sys
 import time
-# import traceback
-# import urllib3
 
 lock = RLock()
-
-# class DateHelper:
-#     @staticmethod
-#     def parse_date(self, message):
-#         date = datetime.today()
-#         matches = DockerHelper.date_pattern.search(message)
-#         if matches is not None:
-#             date.replace(hour=int(matches.group('hour')))\
-#                 .replace(minute=int(matches.group('minute')))\
-#                 .replace(second=int(matches.group('second')))
-#             if matches.group('microsecond'):
-#                 date = date.replace(microsecond=int(matches.group('microsecond'))*1000 if len(matches.group('microsecond')) == 3 else int(matches.group('microsecond')))
-        
-#         return date.strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
-
-#     @staticmethod
-#     def clean_date(self, message):
-#         return DockerHelper.date_pattern.sub("", message)
-
-# class DockerHelper(Thread, ABC):
-#     date_pattern = re.compile('^\[?(\d{4}[-/]\d{2}[-/]\d{2})?T?\s*((?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})(?:[,.]+(?P<microsecond>\d{3}(?:\d{3})?))?)(\s*\+\d{4})?Z?\]?\s*')
-
-#     def __init__(self, client=docker.from_env()):
-#         Thread.__init__(self)
-#         self.client = client
-
-#     @abstractmethod
-#     def run(self):
-#         pass
-
-#     def parse_date(self, message):
-#         date = datetime.today()
-#         matches = DockerHelper.date_pattern.search(message)
-#         if matches is not None:
-#             date.replace(hour=int(matches.group('hour')))\
-#                 .replace(minute=int(matches.group('minute')))\
-#                 .replace(second=int(matches.group('second')))
-#             if matches.group('microsecond'):
-#                 date = date.replace(microsecond=int(matches.group('microsecond'))*1000 if len(matches.group('microsecond')) == 3 else int(matches.group('microsecond')))
-        
-#         return date.strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
-
-#     def clean_date(self, message):
-#         return DockerHelper.date_pattern.sub("", message)
-
-# class ContainerHelper(DockerHelper):
-
-#     def __init__(self, container, client=docker.from_env()):
-#         DockerHelper.__init__(self, client)
-#         self.container = container
-#         self.skeleton = self.build_sekeleton() 
-
-#     def build_sekeleton(self):
-#         logSkeleton = {}
-#         labels = self.container.labels
-#         # print( json.dumps( labels, ensure_ascii=False, indent=4 ) )
-#         if "com.docker.compose.project" in labels: 
-#             logSkeleton["project"] = labels["com.docker.compose.project"]
-#         if "com.docker.compose.service" in labels: 
-#             logSkeleton["service"] = labels["com.docker.compose.service"]
-#         if "com.docker.compose.container-number" in labels: 
-#             logSkeleton["number"] = labels["com.docker.compose.container-number"]
-
-#         return logSkeleton
-
-#     def run(self):
-#         print("%s : Start following logs" % self.container.name)
-#         self.logs = self.container.logs(stream=True, tail=0)
-#         for log in self.logs:
-#             try:
-#                 formattedLog = dict(self.skeleton)
-#                 formattedLog["date"] = self.parse_date(log.decode("utf-8"))
-#                 formattedLog["message"] = self.clean_date(log.decode("utf-8")).strip()
-
-#                 # TODO : Save to queue
-#                 es = ElasticHelper()
-#                 es.register(formattedLog)
-#                 # with lock:
-#                 #     print( json.dumps( formattedLog, ensure_ascii=False, indent=4 ) )
-#             except:
-#                 print("Exception caught")
-#                 # traceback.print_exc()
-#                 print("=============================================")
-
-
-#     def stop(self):
-#         self.logs.close()
 
 
 class LogAggregator:
     docker = DockerHelper()
     excludes = ['pyckerlogs_logger:latest', 'xylphid/log-pycker:latest', 'docker.elastic.co/elasticsearch/elasticsearch:6.3.1']
     threads = {}
+    status = "running"
 
     def __init__(self):
-        while 1:
-            self.browseContainers()
+        while LogAggregator.status == 'running':
             self.cleanThreads()
+            self.browseContainers()
             with lock:
                 print( 'Threads : %s' % len(LogAggregator.threads) )
             time.sleep(5)
@@ -126,11 +33,6 @@ class LogAggregator:
             tags = [value for value in container.image.attrs["RepoTags"] if value in self.excludes]
             if len(tags):
                 pass
-            elif container.status == "exited":
-                with lock:
-                    print( "Cleaning : %s" % container.name )
-                LogAggregator.threads[container.name].logs.close()
-                LogAggregator.threads[container.name].join()
             elif container.name not in LogAggregator.threads:
                 with lock:
                     print( "Attaching to : %s" % container.name )
@@ -147,12 +49,13 @@ class LogAggregator:
                 LogAggregator.threads[name].join()
 
         # Reduce threads dict
-        self.threads = { name:self.threads[name] for name in self.threads if self.threads[name].is_alive() }
+        LogAggregator.threads = { name:LogAggregator.threads[name] for name in LogAggregator.threads if LogAggregator.threads[name].is_alive() }
         with lock:
             print( 'Threads : %s' % len(LogAggregator.threads) )
 
     @staticmethod
     def terminate():
+        LogAggregator.status = 'terminated'
         for name in LogAggregator.threads:
             with lock:
                 print("- %s :" % name, end='')
@@ -197,14 +100,14 @@ def main():
 
 def terminate(signal, frame):
     try:
-        print( "\nClosing threads : ")
+        print( "\nGracefully stopping threads : ")
         LogAggregator.terminate()
         sys.exit(0)
     except:
         pass
 
 def kill(signal, frame):
-    print( "Kill program...")
+    print( "Program terminated !")
     sys.exit(0)
 
 if __name__ == "__main__":
