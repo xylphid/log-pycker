51dd00ed501c479664d2d502843c1ad90cd90aba
Anthony PERIQUET <anthony@periquet.net>
2018-08-06 16:12:00 +0200

    Refactoring : write code the python way, Remove DockerHelper

--- app/helper/docker_helper.py
+++ app/helper/docker_helper.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-from helper.elasticsearch import ElasticHelper
+from helper.date_helper import *
+from helper.es_helper import ElasticHelper
 from threading import Thread, RLock
 import docker
 import json
@@ -8,56 +9,18 @@ import logging
 import re
 
 lock = RLock()
-logger = logging.getLogger("logpycker")
-
-class DockerHelper:
-    client = docker.from_env()
-    date_pattern = re.compile('\[?(\d{4}[-/]\d{2}[-/]\d{2})?T?\s*((?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})(?:[,.]+(?P<microsecond>\d{3}(?:\d{3})?))?)(\s*\+\d{4})?Z?\]?\s*')
-
-
-    # Return container list
-    @staticmethod
-    def getContainers():
-        try:
-            return DockerHelper.client.containers.list()
-        except docker.errors.APIError:
-            return []
-
-    @staticmethod
-    def hasDate(message):
-        matches = DockerHelper.date_pattern.search( message )
-        return False if matches is None else True
-
-    # Extract date from message
-    @staticmethod
-    def parseDate(message):
-        date = datetime.today()
-        matches = DockerHelper.date_pattern.search( message )
-        if matches is not None:
-            date.replace(hour=int(matches.group('hour')))\
-                .replace(minute=int(matches.group('minute')))\
-                .replace(second=int(matches.group('second')))
-            if matches.group('microsecond'):
-                date = date.replace(microsecond=int(matches.group('microsecond'))*1000 if len(matches.group('microsecond')) == 3 else int(matches.group('microsecond')))
-        
-        return date.strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
-
-    # Delete date from message
-    @staticmethod
-    def cleanDate(message):
-        return DockerHelper.date_pattern.sub("", message)
-
 
 class ContainerHelper(Thread):
     def __init__(self, container, client=docker.from_env()):
         Thread.__init__(self)
+        self.logger = logging.getLogger("logpycker")
         self.client = client
         self.container = container
         self.previousLog = None
         self.logs = []
-        self.skeleton = self.buildSkeleton()
+        self.skeleton = self.build_skeleton()
 
-    def isMultilineEnabled(self):
+    def is_multiline_enabled(self):
         self.container.reload()
         self.labels = self.container.labels
         if "log.pycker.multiline.enabled" in self.labels:
@@ -65,7 +28,7 @@ class ContainerHelper(Thread):
         else:
             return False
 
-    def buildSkeleton(self):
+    def build_skeleton(self):
         logSkeleton = {}
         self.labels = self.container.labels
 
@@ -73,7 +36,6 @@ class ContainerHelper(Thread):
         logSkeleton["networks"] = [ network for network in self.container.attrs["NetworkSettings"]["Networks"] ]
 
         # Identify compose services
-        # logger.debug( json.dumps( self.container.attrs, ensure_ascii=False, indent=4 ) )
         if "com.docker.compose.project" in self.labels: 
             logSkeleton["project"] = self.labels["com.docker.compose.project"]
         if "com.docker.compose.service" in self.labels: 
@@ -94,7 +56,7 @@ class ContainerHelper(Thread):
 
 
     # https://regex101.com/
-    def parseLogPattern(self, formattedLog):
+    def parse_log_pattern(self, formattedLog):
         if "log.pycker.pattern" in self.labels:
             try:
                 pattern = re.compile( self.labels["log.pycker.pattern"] )
@@ -104,25 +66,25 @@ class ContainerHelper(Thread):
                         formattedLog[name] = value
                         formattedLog["message"] = pattern.sub("", formattedLog["message"])
             except:
-                logger.exception( "Error using log pattern : %s" % self.labels["log.pycker.pattern"] )
+                self.logger.exception( "Error using log pattern : %s" % self.labels["log.pycker.pattern"] )
 
         return formattedLog
 
-    def parseMultilineLog(self, message):
-        if not DockerHelper.hasDate(message) and self.previousLog is not None:
+    def parse_multiline_log(self, message):
+        if not has_date(message) and self.previousLog is not None:
             formattedLog = self.previousLog["entry"]
             formattedLog["message"] += message
             if not ElasticHelper().delete( self.previousLog["id"] ):
                 raise
             return formattedLog
         else:
-            return self.parseLog(message)
+            return self.parse_log(message)
 
-    def parseLog(self, message):
+    def parse_log(self, message):
         formattedLog = dict(self.skeleton)
-        formattedLog["date"] = DockerHelper.parseDate(message)
-        formattedLog["message"] = DockerHelper.cleanDate(message)
-        formattedLog = self.parseLogPattern(formattedLog)
+        formattedLog["date"] = parse_date(message)
+        formattedLog["message"] = clean_date(message)
+        formattedLog = self.parse_log_pattern(formattedLog)
 
         return formattedLog
 
@@ -130,17 +92,17 @@ class ContainerHelper(Thread):
         try:
             self.logs = self.container.logs(stream=True, tail=0)
         except:
-            logger.error( "Unable to get logs for %s" % self.container.name )
+            self.logger.error( "Unable to get logs for %s" % self.container.name )
             
         for log in self.logs:
             try:
                 es = ElasticHelper()
                 message = log.decode("utf-8")
                 labels = self.container.labels
-                if self.isMultilineEnabled():
-                    formattedLog = self.parseMultilineLog( message )
+                if self.is_multiline_enabled():
+                    formattedLog = self.parse_multiline_log( message )
                 else:
-                    formattedLog = self.parseLog( message )
+                    formattedLog = self.parse_log( message )
 
                 # TODO : Save to queue ?
                 result = es.register(formattedLog)
@@ -150,4 +112,4 @@ class ContainerHelper(Thread):
                         "entry": formattedLog
                     }
             except:
-                logger.exception("Container Exception")
+                self.logger.exception("Container Exception")
